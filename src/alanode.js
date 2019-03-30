0c5ed5b7f7c1248fc59deb356251cddbc702a2f0
Anton <anton@adc.sh>
2019-03-30 08:03:52 +0300

    add src

---
+++ src/alanode.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { resolve } from 'path'
+import { lstatSync } from 'fs'
+const p = resolve(process.argv[2])
+const [node,,...rest] = process.argv
+const files = rest.map((f) => {
+  try {
+    lstatSync(f)
+    return resolve(f)
+  } catch (err) {
+    try {
+      lstatSync(`${f}.js`)
+      return resolve(f)
+    } catch (er) {
+      try {
+        lstatSync(`${f}.jsx`)
+        return resolve(f)
+      } catch (e) {
+        return f
+      }
+    }
+  }
+})
+process.argv = [node, ...files]
+require(p)
--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-import { debuglog } from 'util'
-
-const LOG = debuglog('alanode')
-
-/**
- * The Binary To Run Node.JS Files With Import And Export Statements.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-export default async function alanode(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('alanode called with %s', text)
-  return text
-}
-
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
+#!/usr/bin/env node
+require('alamode')()
+require('./alanode')
